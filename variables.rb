311798b4abe88cf235849c582b35a8894b157e29
Lucas Colwell <colwellc37@gmail.com>
2022-02-07 20:32:59 -0700

    Add info

---
+++ variables.rb
@@ -0,0 +1,18 @@
+author_name = "Lucas"
+dog_name = "Moose"
+
+# these are integers
+author_age = 27
+dogs_age = 2
+
+# these are floats
+hours_studied = 4.6
+miles_run = 2.5
+
+# These are booleans
+worked_out_today = false
+miles_true_false = false
+
+
+# reassign value of string
+author_name = "Lucas Colwell"
